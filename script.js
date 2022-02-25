c7b4a6d2b0ba61ef32a5ce82ca8d84341199caa7
Heitor <hsbicalho@gmail.com>
2022-02-25 17:35:22 -0300

    requisito 3 valida email

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const query = document.querySelector.bind(document);
 
+function validaEmail() {
+  const regex = /\S+@\S+\.\S+/;
+  const email = query("#login").value;
+  const senha = query("#senha").value;
+  const validEmail = regex.test(email); // https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+  if (validEmail && senha.length > 6) {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos");
+  }
+}
+query("#entrar").addEventListener("click", validaEmail);
